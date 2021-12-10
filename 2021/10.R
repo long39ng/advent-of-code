37368b669e26780211b9e850de0aa615998947b7
long.nguyen <long.nguyen@uni-bielefeld.de>
2021-12-11 00:48:10 +0100

    Refactor Day 10

--- 2021/10.R
+++ 2021/10.R
@@ -12,45 +12,28 @@ ref <- tribble(
   "<",   ">",       25137,        4
 )
 
-str_mask_chunks <- function(str) {
-  str |>
-    stri_replace_all_fixed(ref$open, "0", vectorise_all = FALSE) |>
-    stri_replace_all_fixed(ref$close, "1", vectorise_all = FALSE)
+chunk_regex <- "\\(\\)|\\[\\]|\\{\\}|\\<\\>"
+close_regex <- "\\)|\\]|\\}|\\>"
+
+remove_closed_chunks <- function(str) {
+  while (stri_detect_regex(str, chunk_regex)) {
+    str <- stri_replace_all_regex(str, chunk_regex, "")
+  }
+  str
 }
 
-illegal_close <- function(str) {
-  mask <- str_mask_chunks(str)
-  chunk_mask <- "0v*1"
-
-  while (stri_detect_regex(mask, chunk_mask)) {
-    chunk_pos <- stri_locate_first_regex(mask, chunk_mask)[1,]
-    open <- stri_sub(str, chunk_pos[[1]], length = 1)
-    close <- stri_sub(str, chunk_pos[[2]], length = 1)
-
-    if (ref$close[ref$open == open] != close) return(close)
-
-    stri_sub(mask, chunk_pos[[1]], length = 1) <- "v"
-    stri_sub(mask, chunk_pos[[2]], length = 1) <- "v"
-  }
-  NA_character_
+first_illegal_close <- function(str) {
+  str <- remove_closed_chunks(str)
+  stri_extract_first_regex(str, close_regex)
 }
 
 need_autocomplete <- function(str) {
-  mask <- str_mask_chunks(str)
-  chunk_mask <- "0v*1"
-
-  while (stri_detect_regex(mask, chunk_mask)) {
-    chunk_pos <- stri_locate_first_regex(mask, chunk_mask)[1,]
-    stri_sub(mask, chunk_pos[[1]], length = 1) <- "v"
-    stri_sub(mask, chunk_pos[[2]], length = 1) <- "v"
-  }
-  open_locs <- stri_locate_all_fixed(mask, "0", get_length = TRUE)[[1]][,1]
-
-  strsplit(str, "")[[1]][open_locs]
+  str <- remove_closed_chunks(str)
+  rev(strsplit(str, "")[[1]])
 }
 
 autocomplete_points <- function(str) {
-  tibble(open = rev(need_autocomplete(str))) |>
+  tibble(open = need_autocomplete(str)) |>
     left_join(ref, by = "open") |>
     pull(points2) |>
     reduce(\(x, y) x * 5 + y)
@@ -63,7 +46,7 @@ input <- tibble(x = readLines(here::here("2021/Day10/data.txt")))
 # Part 1 ------------------------------------------------------------------
 
 input |>
-  mutate(illegal = map_chr(x, illegal_close)) |>
+  mutate(illegal = map_chr(x, first_illegal_close)) |>
   count(illegal) |>
   inner_join(ref, by = c("illegal" = "close")) |>
   summarise(result = sum(n * points1))
@@ -71,7 +54,7 @@ input |>
 # Part 2 ------------------------------------------------------------------
 
 input |>
-  mutate(illegal = map_chr(x, illegal_close)) |>
+  mutate(illegal = map_chr(x, first_illegal_close)) |>
   filter(is.na(illegal)) |>
   mutate(points = map_dbl(x, autocomplete_points)) |>
   summarise(result = median(points))
