89380fc419c9dd6da41a1afa310ec323401fe215
long.nguyen <long.nguyen@uni-bielefeld.de>
2023-12-06 22:39:08 +0100

    2023 Day 1 & 2

---
+++ 2023/day01/src/main.rs
@@ -0,0 +1,87 @@
+use utils::read_lines;
+
+fn main() {
+    if let Some(sum1) = sum_calibration_values("./input.txt", parse_line_1) {
+        println!("[PART 1] Sum of calibration values: {}", sum1);
+    }
+
+    if let Some(sum2) = sum_calibration_values("./input.txt", parse_line_2) {
+        println!("[PART 2] Sum of calibration values: {}", sum2);
+    }
+}
+
+fn sum_calibration_values(input_path: &str, line_parser: fn(&str) -> Option<u32>) -> Option<u32> {
+    if let Ok(lines) = read_lines(input_path) {
+        let sum = lines
+            .filter_map(|line| line.ok())
+            .filter_map(|line| line_parser(&line))
+            .sum();
+
+        Some(sum)
+    } else {
+        None
+    }
+}
+
+fn parse_line_1(line: &str) -> Option<u32> {
+    let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+
+    let first_digit = digits.next().unwrap();
+    let last_digit = digits.last().unwrap_or(first_digit);
+
+    Some(first_digit * 10 + last_digit)
+}
+
+const DIGIT_WORDS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn parse_line_2(line: &str) -> Option<u32> {
+    let first_digit = (0..line.len())
+        .find_map(|pos| parse_digit_at(line, pos))
+        .unwrap();
+
+    let last_digit = (0..line.len())
+        .rev()
+        .find_map(|pos| parse_digit_at(line, pos))
+        .unwrap();
+
+    Some(first_digit * 10 + last_digit)
+}
+
+fn parse_digit_at(line: &str, pos: usize) -> Option<u32> {
+    let char_at_pos = line.chars().nth(pos).unwrap();
+
+    match char_at_pos.to_digit(10) {
+        Some(digit) => Some(digit),
+        None => parse_digit_word(&line[pos..]),
+    }
+}
+
+fn parse_digit_word(s: &str) -> Option<u32> {
+    DIGIT_WORDS
+        .iter()
+        .position(|&word| s.starts_with(word))
+        .map(|index| (index + 1) as u32)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        assert_eq!(
+            sum_calibration_values("./example-1.txt", parse_line_1),
+            Some(142)
+        )
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            sum_calibration_values("./example-2.txt", parse_line_2),
+            Some(281)
+        )
+    }
+}
---
+++ 2023/day02/src/main.rs
@@ -0,0 +1,127 @@
+use std::{collections::HashMap, usize};
+use utils::read_lines;
+
+fn main() {
+    if let Some(sum1) = answer("./input.txt", sum_possible_ids) {
+        println!("[PART 1] Sum of IDs of possible games: {}", sum1)
+    }
+
+    if let Some(sum1) = answer("./input.txt", sum_power) {
+        println!("[PART 2] Sum of IDs of possible games: {}", sum1)
+    }
+}
+
+struct Game {
+    id: usize,
+    sets: Vec<CubeSet>,
+}
+
+struct CubeSet {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+fn answer(input_path: &str, summariser: fn(Vec<Game>) -> usize) -> Option<usize> {
+    if let Ok(lines) = read_lines(input_path) {
+        let games: Vec<Game> = lines
+            .filter_map(|line| line.ok())
+            .map(|line| parse_game(&line))
+            .collect();
+
+        Some(summariser(games))
+    } else {
+        None
+    }
+}
+
+fn sum_possible_ids(games: Vec<Game>) -> usize {
+    games
+        .into_iter()
+        .filter(|game| game_is_possible(game))
+        .map(|game| game.id)
+        .sum()
+}
+
+fn game_is_possible(game: &Game) -> bool {
+    game.sets
+        .iter()
+        .all(|set| set.red <= BAG.red && set.green <= BAG.green && set.blue <= BAG.blue)
+}
+
+fn sum_power(games: Vec<Game>) -> usize {
+    games
+        .iter()
+        .map(|game| max_cube_set(&game.sets))
+        .map(|set| power_cube_set(set))
+        .sum()
+}
+
+fn power_cube_set(set: CubeSet) -> usize {
+    set.red * set.green * set.blue
+}
+
+fn max_cube_set(sets: &Vec<CubeSet>) -> CubeSet {
+    let (max_red, max_green, max_blue) = sets.iter().fold((0, 0, 0), |(red, green, blue), set| {
+        (red.max(set.red), green.max(set.green), blue.max(set.blue))
+    });
+
+    CubeSet {
+        red: max_red,
+        green: max_green,
+        blue: max_blue,
+    }
+}
+
+fn parse_game(line: &str) -> Game {
+    let parts: Vec<&str> = line.split(':').collect();
+
+    Game {
+        id: parts[0].split(' ').collect::<Vec<&str>>()[1]
+            .parse()
+            .unwrap(),
+        sets: parts[1]
+            .trim()
+            .split(';')
+            .map(|s| parse_cube_set(s))
+            .collect(),
+    }
+}
+
+fn parse_cube_set(s: &str) -> CubeSet {
+    let counts: HashMap<&str, usize> = s
+        .split(',')
+        .map(|count_str| {
+            let parts: Vec<&str> = count_str.trim().split(' ').collect();
+
+            (parts[1], parts[0].parse().unwrap())
+        })
+        .collect();
+
+    CubeSet {
+        red: *counts.get("red").unwrap_or(&0),
+        green: *counts.get("green").unwrap_or(&0),
+        blue: *counts.get("blue").unwrap_or(&0),
+    }
+}
+
+const BAG: CubeSet = CubeSet {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        assert_eq!(answer("./example.txt", sum_possible_ids), Some(8))
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(answer("./example.txt", sum_power), Some(2286))
+    }
+}
