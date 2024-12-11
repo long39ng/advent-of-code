83a24d54ad37eab1dc6cf8795b93ba95f5cdd674
Long <hoanglong.nguyen@outlook.de>
2024-12-11 02:41:39 +0100

    2024 Day 5

---
+++ 2024/day05/src/main.rs
@@ -0,0 +1,117 @@
+use std::collections::HashMap;
+use utils::read_lines;
+
+type PageOrderingRule = (u32, u32);
+
+struct Update {
+    pages: Vec<u32>,
+    page_index: HashMap<u32, usize>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (ordering_rules, updates) = read_instructions("input.txt")?;
+
+    let (valid_updates, mut invalid_updates): (Vec<_>, Vec<_>) = updates
+        .into_iter()
+        .partition(|update| update_is_valid(update, &ordering_rules));
+
+    println!("[PART 1] {}", sum_middle_page_numbers(&valid_updates));
+
+    invalid_updates
+        .iter_mut()
+        .for_each(|update| reorder_update(update, &ordering_rules));
+
+    println!("[PART 2] {}", sum_middle_page_numbers(&invalid_updates));
+
+    Ok(())
+}
+
+fn sum_middle_page_numbers(updates: &[Update]) -> u32 {
+    updates
+        .iter()
+        .map(|update| update.pages[update.pages.len() / 2])
+        .sum()
+}
+
+fn update_is_valid(update: &Update, constraints: &[PageOrderingRule]) -> bool {
+    constraints.iter().all(|(a, b)| {
+        if let (Some(a_idx), Some(b_idx)) = (update.page_index.get(a), update.page_index.get(b)) {
+            a_idx < b_idx
+        } else {
+            true
+        }
+    })
+}
+
+fn reorder_update(update: &mut Update, constraints: &[PageOrderingRule]) {
+    let mut is_valid = false;
+
+    while !is_valid {
+        is_valid = true;
+
+        for &(a, b) in constraints {
+            if let (Some(&a_idx), Some(&b_idx)) =
+                (update.page_index.get(&a), update.page_index.get(&b))
+            {
+                if a_idx > b_idx {
+                    is_valid = false;
+
+                    update.pages.swap(a_idx, b_idx);
+
+                    update.page_index.insert(a, b_idx);
+                    update.page_index.insert(b, a_idx);
+                }
+            }
+        }
+    }
+}
+
+fn read_instructions(input_path: &str) -> std::io::Result<(Vec<PageOrderingRule>, Vec<Update>)> {
+    let mut line_iter = read_lines(input_path)?.map_while(Result::ok);
+
+    let ordering_rules = line_iter
+        .by_ref()
+        .take_while(|s| !s.is_empty())
+        .filter_map(|s| {
+            s.split_once('|')
+                .and_then(|(a, b)| Some((a.parse().ok()?, b.parse().ok()?)))
+        })
+        .collect();
+
+    let updates = line_iter
+        .map(|s| {
+            let pages: Vec<_> = s.split(',').filter_map(|x| x.parse().ok()).collect();
+            let page_index = pages
+                .iter()
+                .enumerate()
+                .map(|(i, &page)| (page, i))
+                .collect();
+            Update { pages, page_index }
+        })
+        .collect();
+
+    Ok((ordering_rules, updates))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() -> Result<(), Box<dyn std::error::Error>> {
+        let (ordering_rules, updates) = read_instructions("example.txt")?;
+
+        let (valid_updates, mut invalid_updates): (Vec<_>, Vec<_>) = updates
+            .into_iter()
+            .partition(|update| update_is_valid(update, &ordering_rules));
+
+        assert_eq!(sum_middle_page_numbers(&valid_updates), 143);
+
+        invalid_updates
+            .iter_mut()
+            .for_each(|update| reorder_update(update, &ordering_rules));
+
+        assert_eq!(sum_middle_page_numbers(&invalid_updates), 123);
+        Ok(())
+    }
+}
